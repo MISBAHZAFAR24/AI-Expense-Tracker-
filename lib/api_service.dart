ff89ff8cb2bba5eff6962ddc15a7ba1f7cc33523
MISBAHZAFAR24 <zafarhassan3748@gmail.com>
2026-09-03 16:21:51 +0530

    Updated API base URL and stabilized project

--- lib/api_service.dart
+++ lib/api_service.dart
@@ -4,7 +4,7 @@ import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 
 class ApiService {
-  // Use 10.0.2.2 for Android Emulator to connect to localhost
+
   static const String baseUrl = "http://10.0.2.2:5000/api";
 
   // Helper function to get Token
